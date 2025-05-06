a471d0070ade5c5d001edd96db017117f77a10ba
Solomon Emmanuel <soulex04292003@gmail.com>
2025-05-06 10:16:53 +0100

    chore: added radix-ui dropdown and react-form

--- apps/web/src/app/list/page.tsx
+++ apps/web/src/app/list/page.tsx
@@ -5,7 +5,11 @@ import { useAuthGuard } from '~/hooks/useAuthGuard';
 const page = () => {
   const { isLoading } = useAuthGuard();
   if (isLoading) return <div>please wait.... you'll be prompted to login</div>;
-  return <ListingForm />;
+  return (
+    <div className="max-w-xl mx-auto my-10 px-4">
+      <ListingForm />
+    </div>
+  );
 };
 
 export default page;
